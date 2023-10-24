1449e3c10d2509433595db2be9ecdc8b1a80da17
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-10-23 21:00:16 -0300

    Update API url

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://chbp-api-2150-dev.fl0.io/api'
+export const API_URL = 'https://chbp-backend-dev-sqst.1.us-1.fl0.io/api'
 // export const API_URL = 'https://chbp-api.up.railway.app/api'
 // export const API_URL = 'http://localhost:8080/api'
